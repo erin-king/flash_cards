98e5bfa93d71b20ab783ebd8b3f96f52465a99cf
Erin King <eohone@gmail.com>
2019-01-22 17:35:50 -0700

    added test_it_exists for turn class

---
+++ test/turn_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+
+def test_it_exists
+  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  turn = Turn.new(guess, card)
+
+  assert_instance_of Turn, turn
+end
+
+end
+
+#
+# initialize(string, card) - A turn is initialized with two arguments. The first is a string representing a guess to a card’s question. The second argument is a Card object representing the current flashcard being shown.
+# guess - This method returns the guess
+# card - This method returns the Card
+# correct? - This method returns a boolean indicating if the guess matched the answer on the Card.
+# feedback - This method either returns "Correct!" or "Incorrect." based on whether the guess was correct or not.
+# The Turn class should respond to the following interaction pattern:
+#
+# pry(main)> require './lib/turn'
+# #=> true
+#
+# pry(main)> require './lib/card'
+# #=> true
+#
+# pry(main)> card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# #=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
+#
+# pry(main)> turn = Turn.new("Juneau", card)
+# #=> #<Turn:0x00007f99842f0998 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @guess="Juneau">
+#
+# pry(main)> turn.card
+# #=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
+#
+# pry(main)> turn.guess
+# #=> "Juneau"
+#
+# pry(main)> turn.correct?
+# #=> true
+#
+# pry(main)> turn.feedback
+# #=> "Correct!"
