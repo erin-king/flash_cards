fa4bce06025a30d8344a92f54b3bade2ab4c9afc
Erin King <eohone@gmail.com>
2019-01-24 06:29:01 -0700

    added setup to deck_test, edited test_it_exists

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,16 +6,24 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-  deck = Deck.new(cards)
+    @cards = [@card_1, @card_2, @card_3]
 
-  assert_instance_of Deck, deck
+    @deck = Deck.new(@cards)
+  end
+
+  def test_it_exists
+
+    assert_instance_of Deck, @deck
+  end
+
+
 end
 # Create a Deck class with an accompanying test file. A Deck is initialized with an array of Card objects. A Deck should also be able to return cards based on a given category.
 
