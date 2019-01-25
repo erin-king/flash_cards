8ec0e41764b388c31bc731127f9f8f3b67a374cd
Erin King <eohone@gmail.com>
2019-01-25 06:46:33 -0700

    added more assertions for test_it_can_find_cards_by_category

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -37,13 +37,8 @@ class DeckTest < Minitest::Test
   def test_it_can_find_cards_by_category
 
     assert_equal [@card_2, @card_3], @deck.cards_in_category(:STEM)
+    assert_equal [@card_1], @deck.cards_in_category(:Geography)
+    assert_equal [], @deck.cards_in_category("Pop Culture")
   end
 
 end
-
-
-# pry(main)> deck.cards_in_category(:Geography)
-# #=> [#<Card:0x00007fa16104e160...>]
-#
-# pry(main)> deck.cards_in_category("Pop Culture")
-# #=> []
