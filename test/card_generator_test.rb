638694f9c88fb6ad5ef6c390ddcd032205ab2361
Erin King <eohone@gmail.com>
2019-01-27 12:41:52 -0700

    deleted unneeded commented out code and comments

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -18,12 +18,3 @@ class CardGeneratorTest < Minitest::Test
     assert_equal 4, cards.count
   end
 end
-
-# pry(main)> filename = "cards.txt"
-#=> "cards.txt"
-
-# pry(main)> cards = CardGenerator.new(filename).cards
-#=> [#<Card:0x007f9f1413cbe8 @answer="10", @question="What is 5 + 5?", @category="STEM">,
- #<Card:0x007f9f1413c788 @answer="red panda", @question="What is Rachel's favorite animal?", @category="Turing Staff">,
- #<Card:0x007f9f1413c2b0 @answer="nobody knows", @question="What is Mike's middle name?", @category="Turing Staff">,
- #<Card:0x007f9f14137da0 @answer="Justin bieber", @question="What cardboard cutout lives at Turing?", @category="Pop Culture">]
