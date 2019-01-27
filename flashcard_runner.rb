11595d4ba4ac1459e94b37124dcc985cc5227632
Erin King <eohone@gmail.com>
2019-01-27 12:29:53 -0700

    created two rounds, one with cards.txt, the second with iteration 3 hardcoded cards

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,20 +4,27 @@ require './lib/deck'
 require './lib/round'
 require './lib/card_generator'
 
-# card_1 = Card.new("1 + 1", "2", :Math)
-# card_2 = Card.new("2 + 2", "4", :Math)
-# card_3 = Card.new("0 + 1", "1", :Math)
-# card_4 = Card.new("What is I lowercase?", "i", :Letters)
-# card_5 = Card.new("What is N lowercase?", "n", :Letters)
-#
-# cards = [card_1, card_2, card_3, card_4, card_5]
-
 filename = "cards.txt"
 
-cards = CardGenerator.new(filename).cards
+cards_1 = CardGenerator.new(filename).cards
 
-deck_1 = Deck.new(cards)
+deck_1 = Deck.new(cards_1)
 
 round_1 = Round.new(deck_1)
 
 round_1.start
+
+# Hardcoded cards for iteration 3- now Round 2~
+card_1 = Card.new("1 + 1", "2", :Math)
+card_2 = Card.new("2 + 2", "4", :Math)
+card_3 = Card.new("0 + 1", "1", :Math)
+card_4 = Card.new("What is I lowercase?", "i", :Letters)
+card_5 = Card.new("What is N lowercase?", "n", :Letters)
+
+cards_2 = [card_1, card_2, card_3, card_4, card_5]
+
+deck_2 = Deck.new(cards_2)
+
+round_2 = Round.new(deck_2)
+
+round_2.start
